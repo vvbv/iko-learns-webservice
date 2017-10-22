4b6f7be1563458296ccbf89c879cdd5ab2ff8a73
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2017-10-21 22:15:41 -0500

    Módulo de busca y encuentra.

---
+++ busca_encuentra/obtenerPalabra.php
@@ -0,0 +1,18 @@
+<?php
+    include '../config.php';
+    header('Content-Type: text/html; charset=ISO-8859-1');
+    // Consulta
+    $sql = "SELECT * FROM `palabras` ORDER BY RAND() LIMIT 1";
+    $result = $conn->query($sql);
+    //Manejo de resultados
+    $retorno = $result->fetch_assoc();
+
+    echo json_encode(
+        array(
+            'id' => $retorno['id'],
+            'palabra' => $retorno['palabra_ingles'],
+            'palabra_yolo' => $retorno['palabra_ingles_yolo']
+        )
+    );
+    
+?>
