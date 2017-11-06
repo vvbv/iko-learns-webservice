c9a0001952f64fd268bc510ecef5c716b8065183
Victor Cifuentes <victor.ivan.cifuentes@gmail.com>
2017-11-06 00:43:35 -0500

    Scripts to request services

---
+++ Scripts/Trivias.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+using System.Text;
+
+ [SerializeField]
+class Pregunta{
+	public string id;
+	public string pregunta;
+	public string opcion_a;
+	public string opcion_b;
+	public string opcion_c;
+	public string opcion_d;
+	public string respuesta_correcta;
+}
+
+public class Trivias : MonoBehaviour
+{
+  public string server = "http://localhost/iko-learns/trivias";
+
+  private IEnumerator requestObtenerTrivia()
+  {
+  	UnityWebRequest request = UnityWebRequest.Get(server + "/obtenerTrivia.php");
+  	yield return request.Send();
+
+  	debugRequest(request);
+  }
+
+  private  IEnumerator requestObtenerRetosDisponibles()
+  {
+    UnityWebRequest request = UnityWebRequest.Get(server + "/obtenerRetosDisponibles.php");
+  	yield return request.Send();
+
+  	debugRequest(request);
+  }
+
+
+  private  IEnumerator requestCrearReto(string id_retador, string id_retado)
+  {
+    UnityWebRequest request = UnityWebRequest.Get(server + "/crearReto.php?id_retador="+ id_retador +"&id_retado=" + id_retado);
+  	yield return request.Send();
+
+  	debugRequest(request);
+  }
+
+  private  IEnumerator requestAceptarReto(string id_reto, string id_usuario)
+  {
+    UnityWebRequest request = UnityWebRequest.Get(server + "/aceptarReto.php?id_reto="+ id_reto +"&id_usuario=" + id_usuario);
+  	yield return request.Send();
+
+  	debugRequest(request);
+  }
+
+	private debugRequest(UnityWebRequest request){
+		print("request completed with code: " + request.responseCode);
+
+		if (request.isNetworkError)
+		{
+			print("Error: " + request.error);
+		}
+		else
+		{
+			print("Request Response: " + request.downloadHandler.text);
+		}
+	}
+
+
+
+  public void ObtenerRetosDisponibles()
+  {
+    StartCoroutine(requestObtenerRetosDisponibles());
+  }
+
+	public void ObtenerTrivia()
+	{
+		StartCoroutine(requestObtenerTrivia());
+	}
+
+  public void CrearReto(string id_retador, string id_retado)
+  {
+    StartCoroutine(requestCrearReto(id_retador, id_retado));
+  }
+
+  public void AceptarReto(string id_reto, string id_usuario)
+  {
+    StartCoroutine(requestObtenerRetosDisponibles(id_reto, id_usuario));
+  }
+
+
+}
---
+++ Scripts/Usuarios.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+using System.Text;
+
+public class Usuarios : MonoBehaviour
+{
+  public string server = "http://localhost/iko-learns/usuarios";
+
+  private IEnumerator requestObtenerUsuario(string id_fb)
+  {
+  	UnityWebRequest request = UnityWebRequest.Get(server + "/obtenerUsuario.php?id_fb=" + id_fb);
+  	yield return request.Send();
+
+  	debugRequest(request);
+  }
+
+  private  IEnumerator requestRegistrarPuntos(string id_usuario, string puntos, string accion)
+  {
+    UnityWebRequest request = UnityWebRequest.Get(server + "/registrarPuntos.php?id_usuario="+ id_usuario +"&puntos="+ puntos +"&accion"+ accion);
+  	yield return request.Send();
+
+  	debugRequest(request);
+  }
+
+	private debugRequest(UnityWebRequest request){
+		print("request completed with code: " + request.responseCode);
+
+		if (request.isNetworkError)
+		{
+			print("Error: " + request.error);
+		}
+		else
+		{
+			print("Request Response: " + request.downloadHandler.text);
+		}
+	}
+
+
+
+  public void ObtenerUsuario(string id_fb)
+  {
+    StartCoroutine(requestObtenerRetosDisponibles(id_fb));
+  }
+
+	public void RegistrarPuntos(string id_usuario, string puntos, string accion)
+	{
+		StartCoroutine(requestObtenerTrivia(id_usuario, puntos, accion));
+	}
+
+
+
+
+}
