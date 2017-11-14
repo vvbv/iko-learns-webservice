c3d5f686c86e6275c2fd0374b48893f8ac4c2f39
Oscar <oscar.florez.maya@correounivalle.edu.co>
2017-11-13 23:02:29 -0500

    marcartriviaresuelta.php esta full

--- trivias/marcarTriviaResuelta.php
+++ trivias/marcarTriviaResuelta.php
@@ -1,14 +1,21 @@
 <?php
     include '../config.php';
-    //header('Content-Type: application/json; charset=ISO-8859-1');
 
     // Consulta
-    if(isset($_GET["id_usuario"]) && isset($_GET["id_trivia"])){
+	if(isset($_GET["id_usuario"]) && isset($_GET["id_trivia"])){
+		$id_usuario = $_GET['id_usuario']; 
+		$id_trivia = $_GET['id_trivia'];
 
-        $id_usuario = 'id_usuario';
-        $id_trivia = 'id_trivia';
+		//verificamos que esta trivia no se halla completado antes.
+		$sql = "SELECT * 
+		FROM `trivias_solo_resueltas` 
+		WHERE `id_usuario` = '$id_usuario' and `id_trivia` = '$id_trivia'";
+        
+        $result = $conn->query($sql);
 
-		$sql = "INSERT INTO `trivias_solo_resueltas` (`ìd`,`id_usuario`,`id_trivia`) VALUES('NULL','$id_usuario','$id_trivia')";
-	}
+        if($result->fetch_assoc() == null){
+			$sql = "INSERT INTO `trivias_solo_resueltas` (`id_usuario`,`id_trivia`) VALUES('$id_usuario','$id_trivia')";
+			$conn->query($sql);
+		}
+	}else{}
 ?>
-
