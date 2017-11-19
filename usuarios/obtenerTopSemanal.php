de2008fc1d194f281b1cd0369ac24444099bd1c0
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2017-11-19 00:22:20 -0500

    Implementación de TOP semanal.

---
+++ usuarios/obtenerTopSemanal.php
@@ -0,0 +1,20 @@
+<?php
+    include '../config.php';
+    header('Content-Type: application/json; charset=ISO-8859-1');
+    
+    $sql = "SELECT * FROM puntos INNER JOIN usuarios ON usuarios.id = puntos.id_usuario  WHERE puntos.fecha_hora_Registro BETWEEN date_sub(now(),INTERVAL 1 WEEK) and now() ORDER BY puntos DESC";
+    $result = $conn->query($sql);
+    $top = array();
+    while($row = $result->fetch_assoc()){
+        array_push(
+            $top, 
+            array(
+                'id_usuario' => $row['id_facebook'],
+                'puntos' => $row['puntos']
+            )
+        );
+    };
+
+    echo json_encode($top);
+        
+?>
